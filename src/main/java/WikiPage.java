746666a73eae37e5bae65c8df3d4044d827fff96
Samuel Kogler <samuel.kogler@gmail.com>
2017-05-18 12:58:44 +0200

    xml reader stuff

---
+++ src/main/java/WikiPage.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+
+public class WikiPage implements Serializable {
+    private String title;
+    private String text;
+
+    public WikiPage(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
--- src/main/java/WikiXmlReader.java
+++ src/main/java/WikiXmlReader.java
@@ -1,21 +1,52 @@
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 
 import javax.xml.stream.*;
+import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 public class WikiXmlReader {
     private InputStream inputStream;
     private BZip2CompressorInputStream compressorInputStream;
     private XMLEventReader xmlEventReader;
 
+    private Set<String> linkVocabulary;
+
+
     public WikiXmlReader(InputStream inputStream) throws IOException, XMLStreamException {
         this.inputStream = inputStream;
         compressorInputStream = new BZip2CompressorInputStream(inputStream);
         XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
         xmlEventReader = xmlInputFactory.createXMLEventReader(compressorInputStream);
+    }
+
+    public void findVocabulary() throws XMLStreamException {
+        int numPages = 0;
+
+        boolean inPage = false;
+        while (xmlEventReader.hasNext()) {
+            XMLEvent xmlEvent = xmlEventReader.nextEvent();
+            if (xmlEvent.isStartElement()) {
+                StartElement startElement = xmlEvent.asStartElement();
+                if ("page".equals(startElement.getName().getLocalPart())) {
+                    numPages++;
+                    inPage = true;
+                }
+
+            } else if (xmlEvent.isEndElement()) {
+                EndElement endElement = xmlEvent.asEndElement();
+                if ("page".equals(endElement.getName().getLocalPart())) {
+                    inPage = false;
+                }
+            }
+        }
+        System.out.println("Parsed " + numPages + " pages");
     }
 
     public void iteratePages() throws XMLStreamException {
@@ -28,13 +59,22 @@ public class WikiXmlReader {
                 if (startElement.getName().getLocalPart().equalsIgnoreCase("page")) {
                     numPages++;
                 }
+
             }
         }
         System.out.println("Parsed " + numPages + " pages");
-
     }
 
     public void close() throws IOException {
         compressorInputStream.close();
+    }
+
+    public List<String> extractLinks(String text)
+    {
+        ArrayList<String> links = new ArrayList<>();
+
+
+
+
     }
 }
